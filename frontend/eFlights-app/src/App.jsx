ebab6c1d85e4607584f7c437080dcbbd425dcf9e
Didac Fernandez <didac.fernandez@insiemecloud.net>
2023-03-26 18:06:24 -0700

    Added some packages

--- frontend/eFlights-app/src/App.jsx
+++ frontend/eFlights-app/src/App.jsx
@@ -1,5 +1,6 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
