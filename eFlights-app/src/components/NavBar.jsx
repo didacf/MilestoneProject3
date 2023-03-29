ae2ccd9e64d7aa8f4ba9deb88323167dc47361ee
Jkishi6 <jonathankishi6@gmail.com>
2023-03-29 14:44:12 -0700

    basic navbar created

---
+++ eFlights-app/src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react'
+
+import styles from "./NavBar.module.scss"
+
+import { BiMenuAltRight } from "react-icons/bi"
+import { AiOutlineCloseSquare, AiOutlineShoppingCart } from "react-icons/ai"
+
+
+const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const menuToggler = () => setMenuOpen((p) => !p);
+
+
+  return (
+    <div className={styles.header}>
+        <div className={styles.header__content}>
+            <div>
+                <a className={styles.logo} href={"./"}>eFlights</a>
+            </div>
+            <div>
+                <nav className={`${styles.nav} ${menuOpen ? styles[`nav--open`] : {}}`}>
+                    <a className={styles.nav__item} href={"/"}>Flights</a>
+                    <a className={styles.nav__item} href={"/"}>About</a>
+                    <a className={styles.nav__item} href={"/"}>Contact</a>
+                    <a className={styles.nav__item} href={"/"}>Cart</a>
+                </nav>
+            </div>
+            <div>
+                
+                <button className={styles.header__toggler} onClick={menuToggler}>
+                    {!menuOpen ? <BiMenuAltRight /> : <AiOutlineCloseSquare />}
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+
+
+
+export default NavBar
