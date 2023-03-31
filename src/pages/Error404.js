a2a060c7cdfcc5146e2e1b6b8eba509b1ebffb76
Jkishi6 <jonathankishi6@gmail.com>
2023-03-30 21:53:56 -0700

    error404 page added

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
 import styles from '../styles/Error404.module.scss'
 
 const Error404 = () => {
   return (
     <>
-      <NavBar/>
       <div className={styles.container}>
         <h1 className={styles.content}>Error404</h1>
         <h2 className={styles.content}>Oops! This Page Does Not Exist!</h2>
+        <a href='./' className={styles.content__button}>Return Home</a>
       </div>
     </>
   )
