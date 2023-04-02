fc42e7b372997ea67481751db9cb632f7575eef0
Jkishi6 <jonathankishi6@gmail.com>
2023-04-02 12:19:26 -0700

    basic homepage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <Login/>
+    <Home/>
   );
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from './../styles/Search.module.scss'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input className={styles.form__item} type={"text"} placeholder='From'/>
+        <input className={styles.form__item} type={"text"} placeholder='To'/>
+        <input className={styles.form__date} type={"date"} placeholder='Depart Date'/>
+        <input className={styles.form__date} type={"date"} placeholder='Return Date'/>
+        <input type={"submit"} value={"Submit"}/>
+      </form>
+    </div>
   )
 }
 
