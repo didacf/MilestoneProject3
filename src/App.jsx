613ce6d3bd87116812754c2e7c640aefd107896c
Jkishi6 <jonathankishi6@gmail.com>
2023-04-02 11:12:30 -0700

    updated styles for login and signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <Signup/>
+    <Login/>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import styles from "../styles/Signup.module.scss"
+import { useState } from 'react'
 
 const Login = () => {
+
+  const [userData, setUserData] = useState({email:'', password:''})
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(userData)
+  }
+
   return (
     <>
       <NavBar/>
         <div className={styles.container}>
             
-            <form className={styles.form}>
+            <form onSubmit={(e) => handleSubmit(e, userData)} className={styles.form}>
               <h2 className={styles.form__title}>Login</h2>
-              <input type={"text"} id="email" name="email" className={styles.form__item} placeholder='Email'/>
-              <input type={"text"} id="password" name="password" className={styles.form__item} placeholder='Password'/>
+              <input type={"text"} id="email" name="email" className={styles.form__item} placeholder='Email'
+                value = {userData.email}
+                onChange={e => setUserData({...userData, email: e.target.value})}
+              />
+              <input type={"password"} id="password" name="password" className={styles.form__item} placeholder='Password'
+              value = {userData.password}
+              onChange={e => setUserData({...userData, password: e.target.value})}
+              />
               <div className={styles.form__buttonContainer}>  
                 <input type={"submit"} className={styles.form__button} value="Login"/>
               </div>
