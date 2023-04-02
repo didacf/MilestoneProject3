f644979ed8573638685030a48d65a1b64dad5328
Jkishi6 <jonathankishi6@gmail.com>
2023-04-02 10:44:37 -0700

    updated signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Error404 from './pages/Error404';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <Error404/>
+    <Signup/>
   );
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,23 +1,45 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import styles from "../styles/Signup.module.scss"
+import { useState, useEffect } from 'react'
 
 const Signup = () => {
+
+  const [userData, setUserData] = useState({fName:'', lName:'', email:'', password:''})
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(userData)
+  }
+
+
   return (
     <>
         <NavBar/>
         <div className={styles.container}>
             
-            <form className={styles.form}>
+            <form onSubmit={(e) => handleSubmit(e, userData)} className={styles.form}>
             <h2 className={styles.form__title}>Signup</h2>                
-                <input type={"text"} id="fName" name="fName" className={styles.form__item} placeholder='First Name'/>                
-                <input type={"text"} id="lName" name="lName" className={styles.form__item} placeholder='Last Name'/> 
+                <input type={"text"} id="fName" name="fName" className={styles.form__item} placeholder='First Name'
+                  value = {userData.fName}
+                  onChange={e => setUserData({...userData, fName: e.target.value})}
+                />                
+                <input type={"text"} id="lName" name="lName" className={styles.form__item} placeholder='Last Name'
+                  value = {userData.lName}
+                  onChange={e => setUserData({...userData, lName: e.target.value})}  
+                /> 
                 
-                <input type={"text"} id="email" name="email" className={styles.form__item} placeholder='Email'/>                
+                <input type={"text"} id="email" name="email" className={styles.form__item} placeholder='Email'
+                  value = {userData.email}
+                  onChange={e => setUserData({...userData, email: e.target.value})}
+                />                
                 <input type={"text"} id="conEmail" name="conEmail" className={styles.form__item} placeholder='Confirm Email'/>                
                 
-                <input type={"text"} id="password" name="password" className={styles.form__item} placeholder='Password'/>  
-                <input type={"text"} id="conPassword" name="conPassword" className={styles.form__item} placeholder='Confirm Password'/>
+                <input type={"password"} id="password" name="password" className={styles.form__item} placeholder='Password'
+                  value = {userData.password}
+                  onChange={e => setUserData({...userData, password: e.target.value})}
+                />  
+                <input type={"password"} id="conPassword" name="conPassword" className={styles.form__item} placeholder='Confirm Password'/>
                 <div className={styles.form__buttonContainer}>
                 <input type={"submit"} className={styles.form__button} value="Signup"/>
                 </div>
