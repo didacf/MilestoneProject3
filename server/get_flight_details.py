17094a6b40827acff10413c8b28a7865cca73671
Didac Fernandez <didac.fernandez@insiemecloud.net>
2023-04-02 19:41:00 -0700

    Deleted some files and added routes

--- server/get_flight_details.py
+++ server/get_flight_details.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://skyscanner50.p.rapidapi.com/api/v1/getFlightDetails"
 
-querystring = {"itineraryId":"13416-2304031800--32317-0-13870-2304041405|13870-2304081235--32317-0-13416-2304081600","legs":"[{\"origin\":\"LAX\",\"destination\":\"MAD\",\"date\":\"2023-04-03\"},{\"date\":\"2023-04-08\",\"destination\":\"LAX\",\"origin\":\"MAD\"}]","adults":"1","currency":"USD","countryCode":"US","market":"en-US"}
+querystring = {"itineraryId":"13416-2304032045--32540-1-13870-2304042345|13870-2304140940--32540-2-13416-2304142245","legs":"[{\"origin\":\"LAX\",\"destination\":\"MAD\",\"date\":\"2023-04-03\"},{\"date\":\"2023-04-14\",\"destination\":\"LAX\",\"origin\":\"MAD\"}]","adults":"1","currency":"USD","countryCode":"US","market":"en-US"}
 
 headers = {
 	"X-RapidAPI-Key": "673cd5f30amsh23e878dc6ccc272p1223f6jsn720ad90640aa",
