d308a3f0bf6e2cf5e9a318f7e871085016f0bf30
Jkishi6 <jonathankishi6@gmail.com>
2023-04-06 15:39:46 -0700

    clicking button makes things disappear

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import Search from './components/Search'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Error404 from './pages/Error404';
-
+import Home from "./pages/Home";
+import Search from "./components/Search";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Error404 from "./pages/Error404";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div>
-    <Router>
-  <Routes>  
-  <Route path="/" element={<Home/>}/>
-  <Route path="/search" element={<Search />} />
-  <Route path="/Login" element={<Login />} />
-  <Route path="/Signup" element={<Signup />} />
-  <Route path='/*' element={<Error404 />} />
-  </Routes>
-  </Router>
-  </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/*" element={<Error404 />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
